7c81e409f277cb4262bfdd21409d89021f68ff36
Pavel Šmejkal <smejkal.pa@gmail.com>
2018-01-12 11:35:56 +0100

    Initial commit

---
+++ 01/src/Camera.h
@@ -0,0 +1,142 @@
+#pragma once
+#include <DirectXMath.h>
+#include <iostream>
+
+namespace dx = DirectX;
+
+enum class CameraMovement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+};
+
+constexpr float YAW = -90.0f;
+constexpr float PITCH = 0.0f;
+constexpr float SPEED = 2.5f;
+constexpr float SENSITIVTY = 0.1f;
+constexpr float ZOOM = 45.0f;
+
+class Camera {
+public:
+    // Camera Attributes
+    dx::XMFLOAT3 Position;
+    dx::XMFLOAT3 Front;
+    dx::XMFLOAT3 Up;
+    dx::XMFLOAT3 Right;
+    dx::XMFLOAT3 WorldUp;
+
+    // Eular Angles
+    float Yaw;
+    float Pitch;
+    
+    // Camera options
+    float MovementSpeed;
+    float MouseSensitivity;
+    float Zoom;
+
+    // Constructor with vectors
+    Camera(
+            dx::XMFLOAT3 position = dx::XMFLOAT3(0.0f, 0.0f, 0.0f), 
+            dx::XMFLOAT3 up = dx::XMFLOAT3(0.0f, 1.0f, 0.0f), 
+            float yaw = YAW,
+            float pitch = PITCH)
+            : Front(dx::XMFLOAT3(0.0f, 0.0f, -1.0f))
+            , MovementSpeed(SPEED)
+            , MouseSensitivity(SENSITIVTY)
+            , Zoom(ZOOM) {
+        Position = position;
+        WorldUp = up;
+        Yaw = yaw;
+        Pitch = pitch;
+        updateCameraVectors();
+    }
+    // Constructor with scalar values
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) 
+            : Front(dx::XMFLOAT3(0.0f, 0.0f, -1.0f))
+            , MovementSpeed(SPEED)
+            , MouseSensitivity(SENSITIVTY)
+            , Zoom(ZOOM) {
+        Position = dx::XMFLOAT3(posX, posY, posZ);
+        WorldUp = dx::XMFLOAT3(upX, upY, upZ);
+        Yaw = yaw;
+        Pitch = pitch;
+        updateCameraVectors();
+    }
+
+    // Returns the view matrix calculated using Eular Angles and the LookAt Matrix
+    dx::XMMATRIX GetViewMatrix() const {
+        const dx::XMVECTOR pos = XMLoadFloat3(&Position);
+        const auto focusPosition = dx::XMVectorAdd(XMLoadFloat3(&Position), XMLoadFloat3(&Front));
+        const dx::XMVECTOR up = XMLoadFloat3(&Up);
+        return dx::XMMatrixLookAtRH(pos, focusPosition, up);
+    }
+    
+    // Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+    void ProcessKeyboard(CameraMovement direction, float deltaTime) {
+        using namespace dx;
+        float velocity = MovementSpeed * deltaTime;
+        if (direction == CameraMovement::FORWARD)
+            XMStoreFloat3(&Position, XMLoadFloat3(&Position) - XMLoadFloat3(&Front) * velocity);
+        if (direction == CameraMovement::BACKWARD)
+            XMStoreFloat3(&Position, XMLoadFloat3(&Position) + XMLoadFloat3(&Front) * velocity);
+        if (direction == CameraMovement::LEFT)
+            XMStoreFloat3(&Position, XMLoadFloat3(&Position) - XMLoadFloat3(&Right) * velocity);
+        if (direction == CameraMovement::RIGHT)
+            XMStoreFloat3(&Position, XMLoadFloat3(&Position) + XMLoadFloat3(&Right) * velocity);
+    }
+
+    // Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+    void ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch = true)
+    {
+        xoffset *= MouseSensitivity;
+        yoffset *= MouseSensitivity;
+
+        Yaw += xoffset;
+        Pitch += yoffset;
+
+        // Make sure that when pitch is out of bounds, screen doesn't get flipped
+        if (constrainPitch)
+        {
+            if (Pitch > 89.0f)
+                Pitch = 89.0f;
+            if (Pitch < -89.0f)
+                Pitch = -89.0f;
+        }
+
+        // Update Front, Right and Up Vectors using the updated Eular angles
+        updateCameraVectors();
+    }
+
+    // Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+    void ProcessMouseScroll(float yoffset)
+    {
+        if (Zoom >= 1.0f && Zoom <= 45.0f)
+            Zoom -= yoffset;
+        if (Zoom <= 1.0f)
+            Zoom = 1.0f;
+        if (Zoom >= 45.0f)
+            Zoom = 45.0f;
+    }
+
+private:
+    // Calculates the front vector from the Camera's (updated) Eular Angles
+    void updateCameraVectors()
+    {
+        using namespace dx;
+        // Calculate the new Front vector
+        XMFLOAT3 front;
+        front.x = cos(XMConvertToRadians(Yaw)) * cos(XMConvertToRadians(Pitch));
+        front.y = sin(XMConvertToRadians(Pitch));
+        front.z = sin(XMConvertToRadians(Yaw)) * cos(XMConvertToRadians(Pitch));
+        XMStoreFloat3(&Front, XMVector3Normalize(XMLoadFloat3(&front)));
+        
+        // Also re-calculate the Right and Up vector
+        const auto frontV = XMLoadFloat3(&Front);
+        const auto worldUpV = XMLoadFloat3(&WorldUp);
+        XMStoreFloat3(&Right, XMVector3Normalize(XMVector3Cross(frontV, worldUpV))); // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+
+        const auto rightV = XMLoadFloat3(&Right);
+        XMStoreFloat3(&Up, XMVector3Normalize(XMVector3Cross(rightV, frontV)));
+    }
+};
---
+++ 01/src/ShaderProgram.h
@@ -0,0 +1,124 @@
+#pragma once
+#include <d3d11_1.h>
+#include <d3dcompiler.h>
+#include <vector>
+
+template <typename TConstBuffer>
+class ShaderProgram {
+    ID3D11Buffer* constantBuffer_;
+    ID3D11VertexShader* vertexShader_;
+    ID3D11PixelShader* pixelShader_;
+    ID3D11InputLayout* inputLayout_;
+
+public:
+    ShaderProgram(
+            ID3D11Device* device, 
+            const WCHAR* vertexPath, 
+            const char* vertexStart, 
+            const WCHAR* pixelPath, 
+            const char* pixelStart, 
+            const std::vector<D3D11_INPUT_ELEMENT_DESC>& layout) {
+        // Compile vertex shader
+        ID3DBlob* pVSBlob = nullptr;
+        auto hr = CompileShaderFromFile(vertexPath, vertexStart, "vs_4_0", &pVSBlob);
+        if (FAILED(hr)) {
+            MessageBox(nullptr, L"The FX file cannot be compiled.  Please run this executable from the directory that contains the FX file.", L"Error", MB_OK);
+            return;
+        }
+        // Create vertex shader
+        hr = device->CreateVertexShader(pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), nullptr, &vertexShader_);
+        if (FAILED(hr)) {
+            pVSBlob->Release();
+            MessageBox(nullptr, L"Failed to create vertex shader", L"Error", MB_OK);
+            return;
+        }
+        // Create input layout
+        hr = device->CreateInputLayout(layout.data(), layout.size(), pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), &inputLayout_);
+        pVSBlob->Release();
+        if (FAILED(hr)) {
+            MessageBox(nullptr, L"Failed to create input layout", L"Error", MB_OK);
+            return;
+        }
+        
+        // Compile pixel shader
+        ID3DBlob* pPSBlob = nullptr;
+        hr = CompileShaderFromFile(pixelPath, pixelStart, "ps_4_0", &pPSBlob);
+        if (FAILED(hr))
+        {
+            MessageBox(nullptr, L"The FX file cannot be compiled.  Please run this executable from the directory that contains the FX file.", L"Error", MB_OK);
+            return;
+        }
+
+        // Create the pixel shader
+        hr = device->CreatePixelShader(pPSBlob->GetBufferPointer(), pPSBlob->GetBufferSize(), nullptr, &pixelShader_);
+        pPSBlob->Release();
+        if (FAILED(hr)) {
+            MessageBox(nullptr, L"Failed to create pixel shader", L"Error", MB_OK);
+            return;
+        }
+
+        // Create constantBuffer
+        D3D11_BUFFER_DESC bufferDesc;
+        ZeroMemory(&bufferDesc, sizeof(bufferDesc));
+        bufferDesc.Usage = D3D11_USAGE_DEFAULT;
+        bufferDesc.ByteWidth = sizeof(TConstBuffer);
+        bufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+        bufferDesc.CPUAccessFlags = 0;
+        hr = device->CreateBuffer(&bufferDesc, nullptr, &constantBuffer_);
+        if (FAILED(hr)) {
+            MessageBox(nullptr, L"Failed to create constant buffer", L"Error", MB_OK);
+            return;
+        }
+    }
+
+    ~ShaderProgram() {
+        vertexShader_->Release();
+        pixelShader_->Release();
+        inputLayout_->Release();
+        constantBuffer_->Release();
+    }
+
+    void use(ID3D11DeviceContext* context) const {
+        context->IASetInputLayout(inputLayout_);
+        context->VSSetShader(vertexShader_, nullptr, 0);
+        context->VSSetConstantBuffers(0, 1, &constantBuffer_);
+        context->PSSetShader(pixelShader_, nullptr, 0);
+        context->PSSetConstantBuffers(0, 1, &constantBuffer_);
+    }
+
+    void updateConstantBuffer(ID3D11DeviceContext* context, const TConstBuffer& newBuffer) {
+        context->UpdateSubresource(constantBuffer_, 0, nullptr, &newBuffer, 0, 0);
+    }
+
+private:
+    static HRESULT CompileShaderFromFile(const WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut) {
+        HRESULT hr = S_OK;
+
+        DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+#ifdef _DEBUG
+        // Set the D3DCOMPILE_DEBUG flag to embed debug information in the shaders.
+        // Setting this flag improves the shader debugging experience, but still allows 
+        // the shaders to be optimized and to run exactly the way they will run in 
+        // the release configuration of this program.
+        dwShaderFlags |= D3DCOMPILE_DEBUG;
+
+        // Disable optimizations to further improve shader debugging
+        dwShaderFlags |= D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+
+        ID3DBlob* pErrorBlob = nullptr;
+        hr = D3DCompileFromFile(szFileName, nullptr, nullptr, szEntryPoint, szShaderModel, dwShaderFlags, 0, ppBlobOut, &pErrorBlob);
+        if (FAILED(hr)) {
+            if (pErrorBlob) {
+                OutputDebugStringA(reinterpret_cast<const char*>(pErrorBlob->GetBufferPointer()));
+                pErrorBlob->Release();
+            }
+            return hr;
+        }
+        if (pErrorBlob) {
+            pErrorBlob->Release();
+        }
+
+        return S_OK;
+    }
+};
