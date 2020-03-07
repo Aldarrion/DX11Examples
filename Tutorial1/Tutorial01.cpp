262eea5f908832660b8d285945b2a6dc677e6a5e
Pavel Šmejkal <smejkal.pa@gmail.com>
2020-03-07 15:31:56 +0100

    Add comments to Tutorial 1

--- Tutorial1/Tutorial01.cpp
+++ Tutorial1/Tutorial01.cpp
@@ -13,7 +13,7 @@ ID3D11DeviceContext*    g_pImmediateContext = nullptr;
 IDXGISwapChain*         g_pSwapChain = nullptr;
 ID3D11RenderTargetView* g_pRenderTargetView = nullptr;
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK wndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     PAINTSTRUCT ps;
     HDC hdc;
 
@@ -37,12 +37,14 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
     return 0;
 }
 
-HRESULT InitWindow( HINSTANCE hInstance, int nCmdShow ) {
+
+// Creates a Windows window which we render to.
+HRESULT initWindow( HINSTANCE hInstance, int nCmdShow ) {
     // Register class
     WNDCLASSEX wcex{};
     wcex.cbSize = sizeof(WNDCLASSEX);
     wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
+    wcex.lpfnWndProc = wndProc;
     wcex.cbClsExtra = 0;
     wcex.cbWndExtra = 0;
     wcex.hInstance = hInstance;
@@ -69,15 +71,17 @@ HRESULT InitWindow( HINSTANCE hInstance, int nCmdShow ) {
     return S_OK;
 }
 
-HRESULT CreateSwapChain(IDXGIFactory* dxgiFactory) {
+// Creates the swapchain for presentation with given DXGIFactory
+HRESULT createSwapChain(IDXGIFactory* dxgiFactory) {
     HRESULT hr = S_OK;
 
-    // Create swap chain
+    // Get a rectangle corresponding to the window so we can crete a swapchain buffers with the appropriate size
     RECT rc;
     GetClientRect(g_hWnd, &rc);
     UINT width = rc.right - rc.left;
     UINT height = rc.bottom - rc.top;
 
+    // Create the most simple description of the swapchain
     DXGI_SWAP_CHAIN_DESC sd{};
     sd.BufferCount = 1;
     sd.BufferDesc.Width = width;
@@ -93,18 +97,22 @@ HRESULT CreateSwapChain(IDXGIFactory* dxgiFactory) {
 
     hr = dxgiFactory->CreateSwapChain(g_pd3dDevice, &sd, &g_pSwapChain);
 
+    if (FAILED(hr))
+        return hr;
+
     // Note this tutorial doesn't handle full-screen swapchains so we block the ALT+ENTER shortcut
-    dxgiFactory->MakeWindowAssociation(g_hWnd, DXGI_MWA_NO_ALT_ENTER);
+    hr = dxgiFactory->MakeWindowAssociation(g_hWnd, DXGI_MWA_NO_ALT_ENTER);
 
     if (FAILED(hr))
         return hr;
 
-    // Create a render target view
+    // The swapchain we created has a backbuffer with the format we specified, take it here
     ID3D11Texture2D* pBackBuffer = nullptr;
     hr = g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
     if (FAILED(hr))
         return hr;
 
+    // From the swapchain's backbuffer create a Render target view so we can set and clear it
     hr = g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_pRenderTargetView);
     pBackBuffer->Release();
     if (FAILED(hr))
@@ -125,12 +133,17 @@ HRESULT CreateSwapChain(IDXGIFactory* dxgiFactory) {
     return hr;
 }
 
-IDXGIAdapter* FindBestAdapter(IDXGIFactory* dxgiFactory, UINT createDeviceFlags, D3D_FEATURE_LEVEL* featureLevels, UINT featureLevelCount) {
-    int numAdapters = 0;
+/* 
+/* Finds an adapter which corresponds to given argumens
+ * If multiple adapters are found the one with the most dedicated memory is returned
+ */
+IDXGIAdapter* findBestAdapter(IDXGIFactory* dxgiFactory, UINT createDeviceFlags, D3D_FEATURE_LEVEL* featureLevels, UINT featureLevelCount) {
     IDXGIAdapter* adapter = nullptr;
     size_t bestAdapterMemory = 0;
     IDXGIAdapter* bestAdapter = nullptr;
     D3D_FEATURE_LEVEL adapterFeatureLevel{};
+
+    int numAdapters = 0;
     for (;SUCCEEDED(dxgiFactory->EnumAdapters(numAdapters, &adapter)); ++numAdapters) {
         
         DXGI_ADAPTER_DESC adapterDesc{};
@@ -163,9 +176,11 @@ IDXGIAdapter* FindBestAdapter(IDXGIFactory* dxgiFactory, UINT createDeviceFlags,
     return bestAdapter;
 }
 
-HRESULT InitDevice() {
+// Acquire and initialize all the DirectX11 resources
+HRESULT initDevice() {
     HRESULT hr = S_OK;
 
+    // DXGI factory is used to find adapters and create a swapchain
     IDXGIFactory* dxgiFactory = nullptr;
     if (FAILED(hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory)))) {
         return hr;
@@ -173,106 +188,56 @@ HRESULT InitDevice() {
 
     UINT createDeviceFlags = 0;
     #ifdef _DEBUG
+        // The debug flag enables validation for the DX calls
         createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
     #endif
 
+    // Feature levels do not match the DX runtime version, 11_0 does not mean DX 11.1
+    // for more info see the wiki https://en.wikipedia.org/wiki/Feature_levels_in_Direct3D
+    // We currently build shaders in SM 5.0 therefore we require FL at least 11.0
     D3D_FEATURE_LEVEL featureLevels[] = {
         D3D_FEATURE_LEVEL_11_1,
         D3D_FEATURE_LEVEL_11_0,
     };
     UINT featureLevelCount = ARRAYSIZE(featureLevels);
 
-    IDXGIAdapter* bestAdapter = FindBestAdapter(dxgiFactory, createDeviceFlags, featureLevels, featureLevelCount);
+    // Try to find the best adapter in the system which corresponds to our settings
+    IDXGIAdapter* bestAdapter = findBestAdapter(dxgiFactory, createDeviceFlags, featureLevels, featureLevelCount);
+    if (!bestAdapter) {
+        return S_FALSE;
+    }
 
+    // Create the DX device with the adapter we found
     if (FAILED(hr = D3D11CreateDevice(bestAdapter, D3D_DRIVER_TYPE_UNKNOWN, nullptr, createDeviceFlags,
         featureLevels, featureLevelCount, D3D11_SDK_VERSION, &g_pd3dDevice, &g_featureLevel, &g_pImmediateContext))) {
-        // Should never happen
+        // Should never happen since the options for device creation should be tested already
         return hr;
     }
 
+    // Adapter is no longer needed by us, all objects which need it (device) hold a reference to it
     bestAdapter->Release();
 
-    CreateSwapChain(dxgiFactory);
+    if (FAILED(hr = createSwapChain(dxgiFactory))) {
+        return hr;
+    }
     dxgiFactory->Release();
 
     return S_OK;
 }
 
-HRESULT InitDeviceAlternative() {
-    HRESULT hr = S_OK;
+// Rendering function to be called each frame
+void render() {
+    // Just clear the backbuffer with a solid color
+    g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, Colors::MidnightBlue);
 
-    UINT createDeviceFlags = 0;
-    #ifdef _DEBUG
-        createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
-    #endif
-
-    D3D_DRIVER_TYPE driverTypes[] = {
-        D3D_DRIVER_TYPE_HARDWARE,
-        D3D_DRIVER_TYPE_WARP,
-        D3D_DRIVER_TYPE_REFERENCE,
-    };
-    UINT numDriverTypes = ARRAYSIZE(driverTypes);
-
-    D3D_FEATURE_LEVEL featureLevels[] = {
-        D3D_FEATURE_LEVEL_11_1,
-        D3D_FEATURE_LEVEL_11_0,
-    };
-    UINT numFeatureLevels = ARRAYSIZE(featureLevels);
-
-    for(UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++) {
-        g_driverType = driverTypes[driverTypeIndex];
-        hr = D3D11CreateDevice(nullptr, g_driverType, nullptr, createDeviceFlags, featureLevels, numFeatureLevels,
-                                D3D11_SDK_VERSION, &g_pd3dDevice, &g_featureLevel, &g_pImmediateContext);
-
-        if (hr == E_INVALIDARG) {
-            // DirectX 11.0 platforms will not recognize D3D_FEATURE_LEVEL_11_1 so we need to retry without it
-            hr = D3D11CreateDevice(nullptr, g_driverType, nullptr, createDeviceFlags, &featureLevels[1], numFeatureLevels - 1,
-                                    D3D11_SDK_VERSION, &g_pd3dDevice, &g_featureLevel, &g_pImmediateContext);
-        }
-
-        if(SUCCEEDED(hr))
-            break;
-    }
-    if(FAILED(hr))
-        return hr;
-
-    // Obtain DXGI factory from device (since we used nullptr for pAdapter above)
-    IDXGIFactory1* dxgiFactory = nullptr;
-    {
-        IDXGIDevice* dxgiDevice = nullptr;
-        hr = g_pd3dDevice->QueryInterface(IID_PPV_ARGS(&dxgiDevice));
-        if (SUCCEEDED(hr)) {
-            IDXGIAdapter* adapter = nullptr;
-            hr = dxgiDevice->GetAdapter(&adapter);
-            if (SUCCEEDED(hr)) {
-                hr = adapter->GetParent(IID_PPV_ARGS(&dxgiFactory));
-                adapter->Release();
-            }
-            dxgiDevice->Release();
-        }
-    }
-    if (FAILED(hr))
-        return hr;
-
-    // Create swap chain
-    IDXGIFactory2* dxgiFactory2 = nullptr;
-    hr = dxgiFactory->QueryInterface(IID_PPV_ARGS(&dxgiFactory2));
-
-    CreateSwapChain(dxgiFactory);
-    dxgiFactory->Release();
-
-    return S_OK;
+    // Call present on the swapchain which flips backbuffer and frontbuffer
+    // the first zero specifies that we do not want to wait for vertical sync
+    // the second zero stands for no flags to the present function.
+    g_pSwapChain->Present(0, 0);
 }
 
-void Render()
-{
-    // Just clear the backbuffer
-    g_pImmediateContext->ClearRenderTargetView( g_pRenderTargetView, Colors::MidnightBlue );
-    g_pSwapChain->Present( 0, 0 );
-}
-
-void CleanupDevice()
-{
+// Releases all the resources
+void cleanupDevice() {
     if(g_pImmediateContext) g_pImmediateContext->ClearState();
     if(g_pRenderTargetView) g_pRenderTargetView->Release();
     if(g_pSwapChain) g_pSwapChain->Release();
@@ -280,27 +245,32 @@ void CleanupDevice()
     if(g_pd3dDevice) g_pd3dDevice->Release();
 }
 
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
-    if (FAILED(InitWindow(hInstance, nCmdShow)))
+// The entry point for Windows applications
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
+    // First, try to initialize a Window, this is just Windows stuff
+    if (FAILED(initWindow(hInstance, nCmdShow)))
         return 0;
 
-    if (FAILED(InitDevice())) {
-        CleanupDevice();
+    // Now try to initialize the DirectX device
+    if (FAILED(initDevice())) {
+        cleanupDevice();
         return 0;
     }
 
-    // Main message loop
+    // Main message loop, again, standard Windows stuff
     MSG msg{};
     while (WM_QUIT != msg.message) {
         if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         } else {
-            Render();
+            // When all messages are processed call our custom rendering function is called
+            render();
         }
     }
 
-    CleanupDevice();
+    // Don't forget to release all the resources we acquired
+    cleanupDevice();
 
     return (int)msg.wParam;
 }
